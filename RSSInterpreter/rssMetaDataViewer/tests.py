62afb1ef2528f0f7ce8239ca4d7f274620b3d002
Philip <philip.b.tyler@gmail.com>
2015-05-20 16:31:06 -0700

    Added tests for RSS parsing logic

--- RSSInterpreter/rssMetaDataViewer/tests.py
+++ RSSInterpreter/rssMetaDataViewer/tests.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from django.core.urlresolvers import reverse
 
+from utility.rssHelpers import feedparserWrapper
 from .models import RssVideo
 
 class RssMetaDataViewerViewTests(SimpleTestCase):
@@ -9,6 +10,42 @@ class RssMetaDataViewerViewTests(SimpleTestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Found")
+
+class RssHelpersTests(SimpleTestCase):
+
+    def setUp(self):
+        self.testEntry = {'media_content': [{
+            'type': 'video/mp4',
+            'duration': "60",
+            'filesize': "61440" # <-- 1 KB/s because 6144 B * (1 KB / 1024 B) / 60 s = 1
+        }],
+        'media_thumbnail': [
+          {'url': u'http://www.img.com/img1.jpg', 'width': u'480', 'height': u'360'},
+          {'url': u'http://www.img.com/img1_small.jpg', 'width': u'180', 'height': u'136'},
+          {'url': u'http://www.img.com/img1_tiny.jpg', 'width': u'80', 'height': u'60'},
+          {'url': u'http://www.img.com/img1_max.jpg', 'width': u'560', 'height': u'480'}
+        ]}
+
+    def test_createRssDictFromEntry_validMetaData_returnValidDict(self):
+        """
+        createRssDictFromEntry should parse the media_content dict, retrieve the type, duration and fileSize
+        then use those strings to create the codec string, duration int and bitrate int
+        """
+        parser = feedparserWrapper()
+        videoDicts = parser.createRssDictFromEntry(self.testEntry)
+        self.assertEqual(videoDicts[0]['codec'], "mp4")
+        self.assertEqual(videoDicts[0]['duration'], 60)
+        self.assertEqual(videoDicts[0]['bitrate'], 1)
+
+    def test_getSmallestThumbnailFromEntry_validImages_returnSmallestImgUrl(self):
+        """
+        getSmallestThumbnailFromEntry should calculate the area of each img and return
+        the url of the smallest one
+        """
+        parser = feedparserWrapper()
+        url = parser.getSmallestThumbnailFromEntry(self.testEntry)
+        self.assertEqual(url, u'http://www.img.com/img1_tiny.jpg')
+
 
 class RssVideoModelTests(SimpleTestCase):
 
